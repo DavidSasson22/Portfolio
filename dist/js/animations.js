07e81aa312bafa43e59193f16801c2982d54f93c
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-06-17 14:34:57 +0300

    animation added

---
+++ dist/js/animations.js
@@ -0,0 +1,44 @@
+const homeA = document.querySelector(`#home-a`);
+const certificateNum = document.querySelector(`#certificateNum`);
+const hoursNum = document.querySelector(`#hoursNum`);
+const projectsNum = document.querySelector(`#projectsNum`);
+
+const myCert = 10;
+const myHours = 1000;
+const myprojects = 27;
+
+document.addEventListener("scroll", () => {
+  if (window.pageYOffset > 160) {
+    homeA.style.left = `0px`;
+  } else homeA.style.left = `75%`;
+});
+
+document.addEventListener("scroll", () => {
+  if (window.pageYOffset > 600) {
+    if (certificateNum.textContent < myCert) {
+      for (let i = 0; i <= myCert; i++) {
+        setTimeout(() => {
+          certificateNum.textContent = i;
+        }, 100 * i);
+      }
+    }
+    if (hoursNum.textContent < myHours) {
+      for (let i = 0; i <= myHours; i += 10) {
+        setTimeout(() => {
+          if (i !== 1000) {
+            hoursNum.textContent = i;
+          } else {
+            hoursNum.textContent = "1000+";
+          }
+        }, 1 * i);
+      }
+    }
+    if (projectsNum.textContent < myprojects) {
+      for (let i = 0; i <= myprojects; i++) {
+        setTimeout(() => {
+          projectsNum.textContent = i;
+        }, 50 * i);
+      }
+    }
+  }
+});
