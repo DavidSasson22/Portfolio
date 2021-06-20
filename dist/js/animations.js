683808449e6129baa88f14a983492ec53910da4c
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-06-20 12:11:38 +0300

    better tech

--- dist/js/animations.js
+++ dist/js/animations.js
@@ -1,4 +1,5 @@
 const homeA = document.querySelector(`#home-a`);
+const homeB = document.querySelector(`#home-b`);
 const certificateNum = document.querySelector(`#certificateNum`);
 const hoursNum = document.querySelector(`#hoursNum`);
 const projectsNum = document.querySelector(`#projectsNum`);
@@ -7,37 +8,46 @@ const myCert = 10;
 const myHours = 1000;
 const myprojects = 27;
 
-let active = true;
+let activeCounter = true;
 
-document.addEventListener("scroll", () => {
-  if (window.pageYOffset > 160) {
-    homeA.style.left = `0px`;
-  } else homeA.style.left = `75%`;
-});
+const observerHome = new IntersectionObserver(
+  function (entries) {
+    if (entries[0].isIntersecting === true) {
+      homeA.style.left = `0px`;
+    }
+  },
+  { threshold: [0.05] }
+);
 
-document.addEventListener("scroll", () => {
-  if (window.pageYOffset > 600 && window.pageYOffset < 650) {
-    if (active) {
-      active = false;
-      for (let i = 0; i <= myCert; i++) {
-        setTimeout(() => {
-          certificateNum.textContent = i;
-        }, 150 * i);
-      }
-      for (let i = 0; i <= myHours; i += 10) {
-        setTimeout(() => {
-          if (i !== 1000) {
-            hoursNum.textContent = i;
-          } else {
-            hoursNum.textContent = "1000+";
-          }
-        }, 1.5 * i);
-      }
-      for (let i = 0; i <= myprojects; i++) {
-        setTimeout(() => {
-          projectsNum.textContent = i;
-        }, 75 * i);
+const myNums = new IntersectionObserver(
+  function (entries) {
+    if (entries[0].isIntersecting === true) {
+      if (activeCounter) {
+        activeCounter = false;
+        for (let i = 0; i <= myCert; i++) {
+          setTimeout(() => {
+            certificateNum.textContent = i;
+          }, 150 * i);
+        }
+        for (let i = 0; i <= myHours; i += 10) {
+          setTimeout(() => {
+            if (i !== 1000) {
+              hoursNum.textContent = i;
+            } else {
+              hoursNum.textContent = "1000+";
+            }
+          }, 1.5 * i);
+        }
+        for (let i = 0; i <= myprojects; i++) {
+          setTimeout(() => {
+            projectsNum.textContent = i;
+          }, 75 * i);
+        }
       }
     }
-  }
-});
+  },
+  { threshold: [0.5] }
+);
+
+observerHome.observe(homeA);
+myNums.observe(homeB);
--- dist/js/skillsAnimation.js
+++ dist/js/skillsAnimation.js
@@ -1,18 +1,22 @@
 const c = document.querySelector(`#skillsC`);
 const python = document.querySelector(`#skillsPython`);
 const html = document.querySelector(`#skillsHtml`);
+const sass = document.querySelector(`#skillsSass`);
 const js = document.querySelector(`#skillsJs`);
 const react = document.querySelector(`#skillsReact`);
 const nodeJs = document.querySelector(`#skillsNode`);
 const mongo = document.querySelector(`#skillsMongo`);
+const linux = document.querySelector(`#skillsLinux`);
 
 const cR = 50;
-const pythonR = 70;
+const pythonR = 75;
 const htmlR = 85;
+const sassR = 85;
 const jsR = 95;
 const reactR = 92;
 const nodeJsR = 88;
 const mongoR = 80;
+const linuxR = 70;
 
 let activate = true;
 
@@ -20,19 +24,34 @@ document.addEventListener("scroll", () => {
   if (window.pageYOffset > 450) {
     console.log(activate);
     if (activate) {
-      console.log(`active`);
       activate = false;
-      for (let i = 0; i <= 100; i++) {
-        setTimeout(() => {
-          if (i <= cR) c.style.width = `${i}%`;
-          if (i <= pythonR) python.style.width = `${i}%`;
-          if (i <= htmlR) html.style.width = `${i}%`;
-          if (i <= jsR) js.style.width = `${i}%`;
-          if (i <= reactR) react.style.width = `${i}%`;
-          if (i <= nodeJsR) nodeJs.style.width = `${i}%`;
-          if (i <= mongoR) mongo.style.width = `${i}%`;
-        }, 25 * i);
-      }
+      setTimeout(() => {
+        c.style.width = `${cR}%`;
+      }, 100);
+      setTimeout(() => {
+        python.style.width = `${pythonR}%`;
+      }, 300);
+      setTimeout(() => {
+        html.style.width = `${htmlR}%`;
+      }, 500);
+      setTimeout(() => {
+        sass.style.width = `${sassR}%`;
+      }, 700);
+      setTimeout(() => {
+        js.style.width = `${jsR}%`;
+      }, 900);
+      setTimeout(() => {
+        react.style.width = `${reactR}%`;
+      }, 1100);
+      setTimeout(() => {
+        nodeJs.style.width = `${nodeJsR}%`;
+      }, 1300);
+      setTimeout(() => {
+        mongo.style.width = `${mongoR}%`;
+      }, 1500);
+      setTimeout(() => {
+        linux.style.width = `${linuxR}%`;
+      }, 1700);
     }
   }
 });
